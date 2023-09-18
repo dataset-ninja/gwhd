6df910cb6e69cec1aa203c05394390b655107bab
vorozhkog <vorozkhog@icloud.com>
2023-09-18 13:13:26 +0100

    dataset release

--- DOWNLOAD.md
+++ DOWNLOAD.md
@@ -1,6 +1,6 @@
 Dataset **GWHD 2021** can be downloaded in [Supervisely format](https://developer.supervisely.com/api-references/supervisely-annotation-json-format):
 
- [Download](Set 'HIDE_DATASET=False' to generate download link)
+ [Download](https://assets.supervisely.com/supervisely-supervisely-assets-public/teams_storage/O/P/Ky/AIh6AjeLZZRmT5VLDh72yAGrmi1e669cQrSTMrzTEr6uasHVHJrJoqK1gkPQMVpN838wdbpyWynMouAFKvZlPoGef1AOVTzIZ72USJnGiJ7XMjeb9LfiQqmFsfdy.tar)
 
 As an alternative, it can be downloaded with *dataset-tools* package:
 ``` bash
--- src/settings.py
+++ src/settings.py
@@ -15,13 +15,13 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "GWHD 2021"
 PROJECT_NAME_FULL: str = "GWHD 2021: Global Wheat Head Dataset 2021"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_4_0()
-APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Agricultural(is_used=False)]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Agricultural(is_used=True)]
 CATEGORY: Category = Category.Agriculture()
 
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
